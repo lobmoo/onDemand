340f6cb28736ab65a0cfdb095741f7c76e732ffa
lobmo <1162431386@qq.com>
2025-03-10 14:27:21 +0800

    fix:修复一处bug

--- common/fastdds_wrapper/DataNode.h
+++ common/fastdds_wrapper/DataNode.h
@@ -40,27 +40,12 @@ class DataNode : public DDSParticipantManager {
  public:
   template <typename T>
   void registerTopicType(const std::string &topicName) {
-    std::lock_guard<std::mutex> lock(topicMutex_);
-    if (topicTypeFactory_.find(topicName) == topicTypeFactory_.end()) {
-      topicTypeFactory_[topicName] = []() { return new T(); };
-    }
+      addTopicDataTypeCreator(topicName, []() { return new T(); });
   }
 
   // ´´½¨ DataWriter
   template <typename T>
   DDSTopicDataWriter<T> *createDataWriter(const std::string topicName) {
-    {
-      std::lock_guard<std::mutex> lock(topicMutex_);
-      auto it = topicTypeFactory_.find(topicName);
-      if (it == topicTypeFactory_.end()) {
-        LOG(error) << "Error: Topic type for '" << topicName << "' not registered!";
-        return nullptr;
-      }
-
-      eprosima::fastdds::dds::TopicDataType *dataType = it->second();
-      addTopicDataTypeCreator(topicName, [dataType]() { return dataType; });
-    }
-
     return DDSParticipantManager::createDataWriter<T>(topicName);
   }
 
@@ -68,18 +53,6 @@ class DataNode : public DDSParticipantManager {
   template <typename T>
   DDSTopicDataReader<T> *createDataReader(
       const std::string topicName, std::function<void(const std::string &, std::shared_ptr<T>)> callback) {
-    {
-      std::lock_guard<std::mutex> lock(topicMutex_);
-      auto it = topicTypeFactory_.find(topicName);
-      if (it == topicTypeFactory_.end()) {
-        LOG(error) << "Error: Topic type for '" << topicName << "' not registered!";
-        return nullptr;
-      }
-
-      eprosima::fastdds::dds::TopicDataType *dataType = it->second();
-      addTopicDataTypeCreator(topicName, [dataType]() { return dataType; });
-    }
-
     return DDSParticipantManager::createDataReader<T>(topicName, callback);
   }
 };
