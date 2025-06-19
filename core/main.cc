fd4456dace1a349c9441671caa53f6ae4b2c4fce
lobmo <1162431386@qq.com>
2025-06-19 16:24:44 +0800

    fix:删除定制化代码

--- core/main.cc
+++ core/main.cc
@@ -1,16 +1,14 @@
 #include <log/logger.h>
-#include "fastdds_wrapper/DDSQosConfigXmlFilter.h"
 #include <string>
 
 
-using namespace tinyxml2;
+
 
 
 int main(int argc, char *argv[])
 {
-    Logger::Instance().Init("log/myapp.log", Logger::console, Logger::info, 60, 5);
-    DDSQosConfigXmlFilter filter;
-    std::string xml = filter.filterQoS("/home/wwk/workspaces/test_demo/core/qosConfig.xml");
-    LOG(info) << "Filtered XML: " << xml;
+    Logger::Instance().Init("");
+  
+    LOG(info) << "Filtered XML:   111";
     return 0;
 }
