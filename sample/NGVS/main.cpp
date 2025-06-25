bfd438f517aabb0944faea7cba980b6e9eefc9b4
lobmo <1162431386@qq.com>
2025-06-25 16:16:26 +0800

    1

--- sample/NGVS/main.cpp
+++ sample/NGVS/main.cpp
@@ -325,9 +325,60 @@ void test1()
 //     while (std::cin.get() != '\n') {
 //     }
 // }
+
+
+
+int testNgvs()
+{
+    std::string xmlContent =
+        readXmlFile("/home/wwk/workspaces/test_demo/sample/NGVS/modelNgvs.xml");
+    auto &parser = dsf::parser::ModelParser::getInstance();  
+    std::string error_text;
+    parser.init(xmlContent, error_text);
+    dsf::ngvs::NgvsSerializer Serializer;
+    std::vector<char> outBuffer;
+    std::unordered_map<std::string, std::string> inData;
+    inData["STD.VAR1.B1"] = "123";
+    inData["STD.VAR1.B2"] = "true";
+    inData["STD.VAR1.B3.C1"] = "789";
+    inData["STD.VAR1.B3.C2[0]"] = "3.13";
+    inData["STD.VAR1.B3.C2[1]"] = "3.14";
+    inData["STD.VAR2[0]"] = "16";
+    inData["STD.VAR2[1]"] = "17";
+    inData["STD.VAR3"] = "18";
+    inData["STD.VAR4"] = "19";
+    inData["STD.VAR5"] = "20";
+
+    std::vector<std::shared_ptr<dsf::parser::TreeNode>> leaves;
+    dsf::parser::ModelDefine model;
+
+  
+    if (!Serializer.serialize("STD.NGVS_S1:1.0", inData, outBuffer)) {
+        LOG(error) << "Serialization failed";
+        return 1;
+    }
+
+    std::unordered_map<std::string, std::string> outData;
+    if (!Serializer.deserialize("STD.NGVS_S1:1.0", outBuffer, outData)) {
+        LOG(error) << "deserialize failed";
+        return 1;
+    }
+
+    for(auto &pair : outData) {
+        LOG(info) << "Key: " << pair.first << ", Value: " << pair.second;
+    }
+
+    while (std::cin.get() != '\n') {
+    }
+
+    return 0;
+}
+
+
 int main(int argc, char *argv[])
 {
-    test1();
+    Logger::Instance().Init("log/myapp.log", Logger::console, Logger::debug, 60, 5);
+    testNgvs();
 
     return 0;
 }
